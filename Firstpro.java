3402b202e259e2e8a5a2b322eee8bfbcbf1bd9ea
8608311411IIIT <sanjeevkumar181998@gmail.com>
2017-03-05 18:23:55 +0530

    Add files via upload

---
+++ Firstpro.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Firstpro
+{
+   public static void main(String []args)
+   {
+     Scanner sc=new Scanner(System.in);
+     int a=sc.nextInt();
+     int b=sc.nextInt();
+     while(b-->0) a++;
+     System.out.println(a);
+    
+   }
+
+}      
+
+
+
+
+
+
+
+
+  
